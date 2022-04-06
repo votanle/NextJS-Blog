4ee993b09705ab895a3dd64ecc2a67a4741856ae
Tấn Lễ <letomlhp@gmail.com>
2022-04-06 17:28:34 +0700

    second-commit

--- pages/createblog.js
+++ pages/createblog.js
@@ -1,66 +1,55 @@
-import {useState,useEffect} from 'react'
-import { v4 as uuidv4 } from 'uuid';
-import {storage,db,serverTimestamp} from '../firebase'
+import { useState, useEffect } from 'react'
+// import { storage, db, serverTimestamp } from '../firebase'
+import Link from 'next/link'
 
-export default function createblog({user}) {
-    const [title,setTitle] = useState('')
-    const [content,setContent] = useState('')
-    useEffect(()=>{      
-            try{
-                 db.collection('blogs').add({
-                  title,
-                  content,
-                  postedBy:user.uid,
-                  createdAt:serverTimestamp()
-              })
-              M.toast({html: 'Blog Created',classes:"green"})   
-            }catch(err){
-                M.toast({html:'error creating blog',classes:"red"})    
-            }
-              
+import {auth, db, serverTimestamp} from '../firebase'
 
-    },[])
+export default function createblog({ user }) {
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+    
 
-    const SubmitDetails = ()=>{
+    const createPost = async (e) => {
         if (!title || !content){
             M.toast({html: 'please add all the fields',classes:"red"})    
             return
         }
-       (error) => {
-        M.toast({html: error.message,classes:"red"}) 
-       }, 
-       () => {
-       
-         uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-           console.log('File available at', downloadURL);
-           setUrl(downloadURL)
-            
-         });
-       }
+        try {
+            db.collection("posts").add({
+             title,
+             content,
+             author: {name: auth.currentUser.displayName, id: auth.currentUser.uid},
+             createAt: serverTimestamp(),
+             updateAt: serverTimestamp(),
+            })
 
-
+            M.toast({ html: 'Blog Created', classes: "green" })
+                } catch (err) {
+                    M.toast({ html: 'Error creating blog', classes: "red" })
+                }
     }
+
     return (
         <div className="input-field rootdiv">
             <h3>Create A Blog !!</h3>
             <input
-            type="text"
-            value={title}
-            placeholder="Title"
-            onChange={(e)=>setTitle(e.target.value)}
-            
+                type="text"
+                value={title}
+                placeholder="Title"
+                onChange={(e) => setTitle(e.target.value)}
+
             />
             <textarea
-             type="text"
-             value={content}
-             placeholder="content"
-             onChange={(e)=>setContent(e.target.value)}
-            
-            />
-             <button className="btn #fb8c00 orange darken-1" onClick={()=>SubmitDetails()}>Submit Post</button>
+                type="text"
+                value={content}
+                placeholder="content"
+                onChange={(e) => setContent(e.target.value)}
 
-             <style jsx>
-                 {`
+            />
+            <Link href="/"><button className="btn #fb8c00 orange darken-1" onClick={() => createPost()}>Submit Post</button></Link>
+
+            <style jsx>
+                {`
                  
                  .rootdiv{
                      margin:30px auto;
@@ -69,7 +58,7 @@ export default function createblog({user}) {
                      text-align:center;
                  }
                  `}
-             </style>
+            </style>
 
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,82 @@
-export default function Home() {
-  return (
-    <div>
-     <h1>I love you </h1>
-    </div>
-  )
+import {db} from '../firebase'
+import {useState} from 'react'
+import { createblog } from '../pages/createblog'
+
+export default function Home({ Allposts, user }) {
+  const [posts, setposts] = useState(Allposts)
+  const [end, setEnd] = useState(false)
+  const loadMore = async () => {
+    const last = posts[posts.length - 1]
+    const res = await db.collection('posts')
+      .orderBy('createdAt', 'desc')
+      .startAfter(new Date(last.createdAt))
+      .limit(6)
+      .get()
+    const newposts = res.docs.map(docSnap => {
+      return {
+        ...docSnap.data(),
+        createdAt: docSnap.data().createdAt.toMillis(),
+        id: docSnap.id
+      }
+    })
+    setposts(posts.concat(newposts))
+    if (newposts.length < 3) {
+      setEnd(true)
+    }
+  }
+  
+    return (
+      <div className="center">
+          {posts.map(post=>{
+            return(
+              <div className="card" key={post.createdAt}>
+              <div className="card-image">
+                <span className="card-title">{post.title}</span>
+              </div>
+              <div className="card-content">
+                <p>{post.content}</p>
+              </div>
+              {/* <div className="card-action">
+                <Link href={`/posts/${post.id}`}><a>Read More</a></Link>
+              </div> */}
+            </div>
+        )
+      })}
+      {end == false ?
+        <button className="btn #FB8C00 blue darken-1" onClick={() => loadMore()}>Load more</button>
+        : <h4 className='show_end'>End</h4>
+      }
+
+          <style jsx>
+             {`
+              .card{
+                max-width:500px;
+                margin:22px auto;
+              }
+              p{
+                display: -webkit-box;
+                overflow: hidden;
+                -webkit-line-clamp: 1;
+                -webkit-box-orient: vertical;
+              }
+             `}
+          </style>
+      </div>
+    )
+  
 }
+export async function getServerSideProps(context) {
+  const querySnap = await db.collectionGroup('posts').orderBy('createdAt', "desc")
+    .get()
+  const Allposts = querySnap.docs.map(docSnap => {
+    return {
+      ...docSnap.data(),
+      createdAt: docSnap.data().createdAt.toMillis(),
+      id: docSnap.id
+    }
+  })
+  return {
+    props: { Allposts }, // will be passed to the page component as props
+  }
+}
+
